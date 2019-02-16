3de256e523ed3438d18760fefc29152396d3279c
Yuta Imai <imai.factory@gmail.com>
2019-02-16 14:49:19 +0900

    Make it run a loop in a rather huristic way

--- src/main.cpp
+++ src/main.cpp
@@ -101,39 +101,69 @@ int main() {
             car_s = end_path_s;
           } 
 
-          bool too_close = false;
-
-          //find ref_v to use
-          for(int i = 0; i < sensor_fusion.size(); i++) {
-
-            //car is in my lane
-            float d = sensor_fusion[i][6];
-            if(d < (2+4*lane+2) && d > (2+4*lane-2)) {
+          // Prediction : Analysing other cars positions.
+          bool car_ahead = false;
+          bool car_left = false;
+          bool car_right = false;
+          for ( int i = 0; i < sensor_fusion.size(); i++ ) {
+              float d = sensor_fusion[i][6];
+              int car_lane = -1;
+              // is it on the same lane we are
+              if ( d > 0 && d < 4 ) {
+                car_lane = 0;
+              } else if ( d > 4 && d < 8 ) {
+                car_lane = 1;
+              } else if ( d > 8 && d < 12 ) {
+                car_lane = 2;
+              }
+              if (car_lane < 0) {
+                continue;
+              }
+              // Find car speed.
               double vx = sensor_fusion[i][3];
               double vy = sensor_fusion[i][4];
-              double check_speed = sqrt(vx*vx+vy*vy);
+              double check_speed = sqrt(vx*vx + vy*vy);
               double check_car_s = sensor_fusion[i][5];
+              // Estimate car s position after executing previous trajectory.
+              check_car_s += ((double)prev_size*0.02*check_speed);
 
-              check_car_s  += ((double)prev_size*.02*check_speed);
-
-              //check s values greater than mine and s gap
-              if((check_car_s > car_s) && (check_car_s-car_s) < 30){
-                too_close = true;
-
-                if(lane == 0) {
-                  lane = 1;
-                } else {
-                  lane = 0;
-                }
+              if ( car_lane == lane ) {
+                // Car in our lane.
+                car_ahead |= check_car_s > car_s && check_car_s - car_s < 30;
+              } else if ( car_lane - lane == -1 ) {
+                // Car left
+                car_left |= car_s - 30 < check_car_s && car_s + 30 > check_car_s;
+              } else if ( car_lane - lane == 1 ) {
+                // Car right
+                car_right |= car_s - 30 < check_car_s && car_s + 30 > check_car_s;
               }
+          }
+
+          // Behavior : Let's see what to do.
+          double speed_diff = 0;
+          const double MAX_SPEED = 49.5;
+          const double MAX_ACC = .224;
+          if ( car_ahead ) { // Car ahead
+            if ( !car_left && lane > 0 ) {
+              // if there is no car left and there is a left lane.
+              lane--; // Change lane left.
+            } else if ( !car_right && lane != 2 ){
+              // if there is no car right and there is a right lane.
+              lane++; // Change lane right.
+            } else {
+              ref_vel -= MAX_ACC;
+            }
+          } else {
+            if ( lane != 1 ) { // if we are not on the center lane.
+              if ( ( lane == 0 && !car_right ) || ( lane == 2 && !car_left ) ) {
+                lane = 1; // Back to center.
+              }
+            }
+            if ( ref_vel < MAX_SPEED ) {
+              ref_vel += MAX_ACC;
             }
           }
 
-          if(too_close) {
-            ref_vel -= .224;
-          } else if(ref_vel < 49.5) {
-            ref_vel += .224;
-          }
 
           // Create a list of widely scoped (x,y) waypoints, evenly spaced at 30m
           // Later we will interoplate these waypoints with a spline and fill it in with more points.
